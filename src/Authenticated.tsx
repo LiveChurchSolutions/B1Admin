298eb3d650b667c60c77731d72bae11c552f3e96
Jeremy Zongker <jeremy@trilitech.com>
2021-01-31 01:51:13 -0600

    Moved B1Admin to its own repo

---
+++ src/Authenticated.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { SettingsPage } from "./settings/SettingsPage";
+import { Header } from "./components";
+import { UserHelper } from "./helpers"
+import UserContext from "./UserContext";
+
+interface Props {
+    location: any;
+}
+
+export const Authenticated: React.FC<Props> = (props) => {
+    var user = React.useContext(UserContext)?.userName; //to force rerender on login
+    return (
+        <>
+            <Header></Header>
+            <div className="container">
+                <Switch>
+                    <Route path="/login"><Redirect to={props.location} /></Route>
+                    <Route path="/settings"><SettingsPage /></Route>
+                </Switch>
+
+            </div>
+            <script src="/js/cp.js"></script>
+        </>
+    );
+}
+
+
